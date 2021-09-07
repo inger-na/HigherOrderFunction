316cd0882ff522f599757728e79a0c3182808b92
inger_na <inger_na@mail.ru>
2021-09-07 13:03:57 +0200

    Exercices Done

--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,18 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract = (fn, ...types) => (...args) => {
+  const tType = types[0].name.toLowerCase();
+  for (let i = 0; i < args.length; i++) {
+    const argsIndex = args[i];
+    const argType = typeof argsIndex;
+    if (tType !== argType)
+      throw new TypeError(`Argument type ${argType} expected ${tType}`);
+  }
+  const res = fn(...args);
+  if (typeof res !== tType)
+    throw new TypeError(`Result type ${tType} expected`);
+  return res;
+};
+
 
 module.exports = { contract };
